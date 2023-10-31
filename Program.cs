630f000f01d135c64665893dc6f73722d73ba718
joao-otaviob <joao.otaviobarros@hotmail.com>
2023-10-31 10:29:05 -0300

    Resolução de desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("::::::::Iniciando Testes com o modelo Nokia::::::");
+Nokia nokia = new("12345", "Modelo 1", "111111111", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhastApp");
+
+
+Console.WriteLine(" ");
+
+Console.WriteLine("::::::::Iniciando Testes com o modelo Iphone::::::");
+Iphone iphone = new("67890", "Modelo 13 PRo", "22222222", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
